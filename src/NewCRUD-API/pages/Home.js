75f1fe99ac4804d21bb4ec030be98ddc55214176
Selvi <reachyourgoal2006@gmail.com>
2023-01-13 21:07:36 +0530

    final

--- src/NewCRUD-API/pages/Home.js
+++ src/NewCRUD-API/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
 import { toast } from "react-toastify";
